382445e7fd0b990209dcd7b1a03c03ebd3ab4dea
Adam Jonas <adamjonas25@gmail.com>
2021-02-03 21:12:00 +0000

    spec for birthdays

--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -3,5 +3,9 @@ require 'birthday_list'
 describe BirthdayList do
   describe '#birthdays' do
     it { is_expected.to respond_to :birthdays }
+
+    it 'it starts empty' do
+      expect(subject.birthdays).to be_empty
+    end
   end
 end
